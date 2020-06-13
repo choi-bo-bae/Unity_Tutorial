b3b028953b5560d630444325c6ae2b06989ef877
choi-bo-bae <qhqo727@gmail.com>
2020-06-13 21:34:54 +0900

    튜토리얼 1 ~ 7 수정 완료

---
+++ Unity_Tutorial/Tutorial2/Assets/Scripts/Test2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    Vector3 rotation;
+
+    [SerializeField] private GameObject go_camera;
+
+    void Start()
+    {
+        rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime;
+            this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime);//이동
+
+            //this.transform.eulerAngles = transform.eulerAngles + new Vector3(90, 0, 0) * Time.deltaTime;
+            //rotation = rotation + new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime);
+            // Debug.Log(transform.eulerAngles);
+
+            this.transform.rotation = Quaternion.Euler(rotation + new Vector3(90, 0, 0) * Time.deltaTime);//회전
+
+            this.transform.localScale = this.transform.localScale + new Vector3(2, 2, 2) * Time.deltaTime;//크게
+
+            this.transform.LookAt(go_camera.transform.position);//카메라를 바라봄
+
+            transform.RotateAround(go_camera.transform.position, Vector3.up, 100 * Time.deltaTime);//카메라 주변을 돎
+        }
+    }
+
+
+
+}
---
+++ Unity_Tutorial/Tutorial3/Assets/Scripts/Test3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test3 : MonoBehaviour
+{
+   
+    private Rigidbody myRigid;
+    //private Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        //rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //myRigid.velocity = new Vector3(0, 0, 1);
+
+            //myRigid.velocity = Vector3.forward;//이동
+            //myRigid.maxAngularVelocity = 100f;
+            //myRigid.angularVelocity = Vector3.right * 100;//회전
+
+            //myRigid.MovePosition(Vector3.forward * Time.deltaTime);
+
+            //rotation = rotation + new Vector3(90, 0, 0) * Time.deltaTime;
+            //myRigid.MoveRotation(Quaternion.Euler(rotation)); //회전
+
+            myRigid.AddForce(Vector3.forward);// 물리영향 받는 이동
+
+            myRigid.AddTorque(Vector3.up);// 물리영향 받는거 회전
+
+            myRigid.AddExplosionForce(10, this.transform.right, 10);//폭발 영향
+
+            //Add가 붙으면 물리효과와 관련있음
+        }
+    }
+
+
+}
